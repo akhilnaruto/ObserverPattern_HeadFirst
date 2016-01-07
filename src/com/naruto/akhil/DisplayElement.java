7ba08c19cb072300e833b6950d7ec8ce69baaeb2
akhilesh <akhil.munna02@gmail.com>
2016-01-07 18:04:07 +0530

    created  observer and subject objects

---
+++ src/com/naruto/akhil/DisplayElement.java
@@ -0,0 +1,6 @@
+package com.naruto.akhil;
+
+
+public interface DisplayElement {
+    void display();
+}
---
+++ src/com/naruto/akhil/WeatherData.java
@@ -0,0 +1,18 @@
+package com.naruto.akhil;
+
+public class WeatherData implements Subject {
+    @Override
+    public void registerObserver(Observer o) {
+
+    }
+
+    @Override
+    public void removeObserver(Observer o) {
+
+    }
+
+    @Override
+    public void notifyObserver() {
+
+    }
+}
