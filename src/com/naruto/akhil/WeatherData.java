8ac91c1e0e3902fd5f2b2fad6650fe4d9c5644e6
akhilesh <akhil.munna02@gmail.com>
2016-01-07 18:27:54 +0530

    some code around weather data

--- src/com/naruto/akhil/WeatherData.java
+++ src/com/naruto/akhil/WeatherData.java
@@ -1,18 +1,46 @@
 package com.naruto.akhil;
 
+import java.util.ArrayList;
+
 public class WeatherData implements Subject {
-    @Override
-    public void registerObserver(Observer o) {
+    private ArrayList observers;
+    private float temperature;
+    private float humidity;
+    private float pressure;
 
+    public WeatherData(){
+        observers = new ArrayList();
     }
 
     @Override
-    public void removeObserver(Observer o) {
-
+    public void registerObserver(Observer observer) {
+        observers.add(observer);
     }
 
     @Override
-    public void notifyObserver() {
+    public void removeObserver(Observer observer) {
+        int indexOfObserver = observers.indexOf(observer);
+        if(indexOfObserver > 0){
+            observers.remove(indexOfObserver);
+        }
+    }
 
+    @Override
+    public void notifyObservers() {
+        for(int i = 0; i < observers.size() ; i++ ){
+            Observer observer = (Observer) observers.get(i);
+            observer.update(temperature , humidity , pressure);
+        }
+    }
+
+    public void measurementsChanged(){
+        notifyObservers();
+    }
+
+    public void  setMeasurements(float temperature , float humidity , float pressure){
+        this.temperature = temperature;
+        this.humidity = humidity;
+        this.pressure = pressure;
+        measurementsChanged();
     }
 }
